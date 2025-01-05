7febe6e81a108099d6830867a6cd531b485f09b8
VarunSethi <varunsethi1121@gmail.com>
2025-01-05 18:01:34 +0530

    SeventhCommitFromLocal

--- sortingBubbleSort.java
+++ sortingBubbleSort.java
@@ -1,5 +1,38 @@
 package SEARCHINGandSORTING;
 
+import java.util.Scanner;
+
 public class sortingBubbleSort {
-    
+    public static void bubbleSort(int array[]){
+        for(int i=0;i<array.length-1;i++){
+            for(int j=0;j<array.length-1-i;j++){
+                if(array[j]>array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the size of your Array: ");
+            int arraySize = sc.nextInt();
+            int sampleArray[] = new int[arraySize];
+            for (int i = 0; i < sampleArray.length; i++) {
+                System.out.print("Enter " + (i + 1) + " element: ");
+                sampleArray[i] = sc.nextInt();
+            }
+            System.out.print("The array that you have entered is: ");
+            for (int i = 0; i < sampleArray.length; i++) {
+                System.out.print(sampleArray[i] + " ");
+            }
+            bubbleSort(sampleArray);
+            System.out.println();
+            System.out.print("The array that you have entered after sorting is: ");
+            for (int i = 0; i < sampleArray.length; i++) {
+                System.out.print(sampleArray[i] + " ");
+            }
+        }
+    }
 }
